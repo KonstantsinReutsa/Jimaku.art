52b90abb987e5603aa4c03656d98992a85657a59
KonstantsinReutsa <82835409+KonstantsinReutsa@users.noreply.github.com>
2022-09-21 10:00:23 +0300

    First commit

---
+++ jimaku_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JimakuSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jimaku_site'
---
+++ jimaku_site/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+
+'''def index(request):
+    return render(request, ".html")'''
+
+
+def home(request):
+    return TemplateResponse(request, "home.html")
+
+def catalog(request):
+    return TemplateResponse(request, "catalog.html")
+
+def profile(request):
+    return TemplateResponse(request, "profile.html")
+
+def player(request):
+    return TemplateResponse(request, "player.html")
